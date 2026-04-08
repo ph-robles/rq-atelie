e3724fa5962a549424cb017d2e9aa4ba98b387bb
ph-robles <ph.robles33@gmail.com>
2026-04-07 21:05:45 -0300

    fix footer new

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,18 +6,20 @@ export default function Footer() {
                     <div className="footer-brand">
                         RQ <em>Ateliê</em>
                     </div>
+
                     <div className="footer-tagline">
                         Crochê e macramê artesanal feito à mão, ponto a ponto.
                     </div>
 
                     <div className="footer-social">
                         <a
-                            href="https://www.instagram.com/"
+                            href="https://www.instagram.com"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
                             Instagram
                         </a>
+
                         <a
                             href="https://wa.me/5511999999999"
                             target="_blank"
@@ -31,18 +33,30 @@ export default function Footer() {
                 <div>
                     <div className="footer-col-title">Ateliê</div>
                     <ul className="footer-links">
-                        <li><a href="#colecao">Coleção</a></li>
-                        <li><a href="#sobre">Sobre</a></li>
-                        <li><a href="#colecao">Encomendas</a></li>
+                        <li>
+                            <a href="#colecao">Coleção</a>
+                        </li>
+                        <li>
+                            <a href="#sobre">Sobre</a>
+                        </li>
+                        <li>
+                            <a href="#colecao">Encomendas</a>
+                        </li>
                     </ul>
                 </div>
 
                 <div>
                     <div className="footer-col-title">Informações</div>
                     <ul className="footer-links">
-                        <li><a href="#">Como comprar</a></li>
-                        <li><a href="#">Prazos</a></li>
-                        <li><a href="#">Trocas</a></li>
+                        <li>
+                            <a href="#">Como comprar</a>
+                        </li>
+                        <li>
+                            <a href="#">Prazos</a>
+                        </li>
+                        <li>
+                            <a href="#">Trocas</a>
+                        </li>
                     </ul>
                 </div>
 
@@ -58,7 +72,9 @@ export default function Footer() {
                                 WhatsApp
                             </a>
                         </li>
-                        <li><a href="#">Instagram DM</a></li>
+                        <li>
+                            <a href="#">Instagram DM</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -70,3 +86,4 @@ export default function Footer() {
         </footer>
     )
 }
+``
