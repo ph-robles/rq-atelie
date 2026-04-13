3d8baf09d2ddf08f1f6e42e07e7fae5792ed3b32
ph-robles <ph.robles33@gmail.com>
2026-04-12 23:45:14 -0300

    create instagram

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const WHATSAPP_NUMBER = "5521976612269"
 export const STORE_NAME = "RQ Ateliê"
 export const STORE_DESCRIPTION =
   "Peças de crochê e macramê feitas à mão, ponto a ponto. Encomendas personalizadas."
-export const INSTAGRAM_URL = "https://www.instagram.com/raquel_ilha24" // ← troque pelo real
+export const INSTAGRAM_URL = "https://www.instagram.com/rq_atelie" // ← troque pelo real
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`
